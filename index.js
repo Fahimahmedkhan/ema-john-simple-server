a90fa3cf93ae77128f8236b73aa84ac16a0a0106
fahimahmedkhan <rolx9999@gmail.com>
2022-11-02 23:34:38 +0600

    Add data database and load

--- index.js
+++ index.js
@@ -11,13 +11,24 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.habxwhg.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        const productCollection = client.db('emaJohn').collection('products');
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(error => console.error(error));
 
 
 app.get('/', (req, res) => {
@@ -26,4 +37,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Ema John Server is running on port:${port}`)
-})
+});
